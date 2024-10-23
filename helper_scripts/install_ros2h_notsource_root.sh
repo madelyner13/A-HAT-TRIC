d286de8953da045662885befffa025aefb75495f
Madelyne Rossmann <madelyne.rossmann13@gmail.com>
2024-10-22 20:47:41 -0400

    adding ros2 installation script

---
+++ helper_scripts/install_ros2h_notsource_root.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update && apt install locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+apt install -y software-properties-common
+add-apt-repository universe
+apt update && apt install curl -y
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+apt update && apt upgrade -y
+apt install -y ros-humble-desktop
+apt install -y ros-dev-tools
+source /opt/ros/humble/setup.bash && echo "source /opt/ros/humble/setup.bash" >> .bashrc
+
+
